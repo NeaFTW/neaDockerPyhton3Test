c9b6e0b7f45d02991d7825cac58bdfeddfea0fa0
Simon MACE <simon.mace@epita.fr>
2016-09-06 17:14:46 +0200

    [SERVER][ADD] Added the server for the project with the json return

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+
+import flask
+from flask import Flask
+app = Flask(__name__)
+
+import os
+import json
+
+@app.route("/")
+def hello():
+    return "This is a server for DeVops"
+
+@app.route("/v1/" + os.environ['GROUPNAME'])
+def toto():
+    jsonresult = {}
+    jsonresult["result"] = "success"
+    jsonresult["data"] = os.environ['GROUPNAME']
+
+    print(jsonresult)
+    return flask.jsonify(jsonresult)
+
+if __name__ == "__main__":
+    app.run()
